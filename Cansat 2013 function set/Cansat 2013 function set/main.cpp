e83272df06c3797abb7f7058a6e9c8ad2c001850
Francis Papineau <francispapineau@gmail.com>
2012-06-16 19:54:32 -0400

    Initial Commit

---
+++ Cansat 2013 function set/Cansat 2013 function set/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Cansat 2013 function set
+//
+//  Created by FRANCIS PAPINEAU on 12-06-16.
+//  Copyright (c) 2012 carleton university. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
